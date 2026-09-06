694ee9ec591334586b50c80918dc002d678b9681
Yashmika-P <yashuyash2505@gmail.com>
2026-09-06 20:14:38 +0530

    Time: 0 ms (100%), Space: 19.3 MB (42.85%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,11 +1,24 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        wealth = [0 for i in range(len(accounts))]
-        for i in range(len(accounts)):
-            sum = 0
-            for j in accounts[i]:
-                sum += j
-            wealth[i] = sum
-        return max(wealth)
+        maxValue = -1
+
+        for customer in accounts:
+            customerWealth = sum(customer)
+
+            if customerWealth > maxValue:
+                maxValue = customerWealth
+        
+        return maxValue
+            
+
+# class Solution:
+#     def maximumWealth(self, accounts: List[List[int]]) -> int:
+#         wealth = [0 for i in range(len(accounts))]
+#         for i in range(len(accounts)):
+#             sum = 0
+#             for j in accounts[i]:
+#                 sum += j
+#             wealth[i] = sum
+#         return max(wealth)
 
         
