4a12c457eeb7bd84a22d482bf777039b71b46f82
Yashmika-P <yashuyash2505@gmail.com>
2026-09-06 19:58:47 +0530

    Time: 0 ms (100%), Space: 19.2 MB (88.09%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,14 +1,21 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        vcount = 0
-        hcount = 0
-        for move in moves:
-            if move == 'L':
-                hcount += 1
-            elif move == 'U':
-                vcount +=1
-            elif move == 'R':
-                hcount -= 1 
-            else:
-                vcount -= 1
-        return hcount == 0 and vcount == 0        
+        if moves.count("L")==moves.count("R") and moves.count("U")==moves.count("D"):
+            return True
+        return False
+        
+
+# class Solution:
+#     def judgeCircle(self, moves: str) -> bool:
+#         vcount = 0
+#         hcount = 0
+#         for move in moves:
+#             if move == 'L':
+#                 hcount += 1
+#             elif move == 'U':
+#                 vcount +=1
+#             elif move == 'R':
+#                 hcount -= 1 
+#             else:
+#                 vcount -= 1
+#         return hcount == 0 and vcount == 0        
